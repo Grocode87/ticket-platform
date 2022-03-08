d9ce8812b269874a489b2774042c1192ab58b04f
Colin Grob <colin.grob87@gmail.com>
2022-03-07 16:55:43 -0800

    added payment cancalled page

--- pages/payment/cancelled.js
+++ pages/payment/cancelled.js
@@ -1,27 +1,39 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 const Cancelled = () => {
-    return (
-        <div>
-        <Head>
-          <title>Koachella 2022</title>
-          <meta name="description" content="" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-  
-        <div className="flex">
-          <div className="w-full lg:w-1/2 m-auto">
-            <Image src="/images/banner.jpg" width="1000" height="400" />
-            <div className="pt-8 text-lg">
-            <p className="font-semibold text-2xl pb-4">Payment Cancelled</p>
-            <p>You have not been charged!</p>
+  const router = useRouter()
 
-            <button className='p-2 rounded-lg bg-blue-400 text-white mt-10'>Return to Home</button>
-            </div>
+    return (
+      <div>
+      <Head>
+        <title>Koachella 2022</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="w-full min-h-screen bg-black text-white ">
+        <div className="w-full lg:w-1/2 m-auto">
+          <div className="w-full sm:w-8/12 pt-10">
+            <Image src="/images/header-basic.png" width={800} height={180} />
+          </div>
+          <div className="pt-12 text-lg">
+            <p className="font-semibold text-2xl pb-4">Payment Cancelled</p>
+            <p className="pb-6">You have not been charged!</p>
+
+            <button
+              onClick={() => router.push("/")}
+              className={
+                "p-2 rounded-lg shadow-md bg-purple-700 text-white mt-8"
+              }
+            >
+              Return To Home
+            </button>
           </div>
         </div>
       </div>
+    </div>
     )
 }
 
