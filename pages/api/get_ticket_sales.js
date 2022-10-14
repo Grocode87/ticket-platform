a80342978f23816e01a14a050e9326d022c96166
Colin Grob <colin.grob87@gmail.com>
2022-10-14 16:30:51 -0700

    fixed dashboard

--- pages/api/get_ticket_sales.js
+++ pages/api/get_ticket_sales.js
@@ -2,26 +2,23 @@ import generateRandomCode from "../../utils/generate";
 import { supabase } from "../../utils/supabaseClient";
 
 const handler = async (req, res) => {
+  const { data } = await supabase.from("tickets_halloween").select();
 
-    const {data} = await supabase
-    .from("tickets")
-    .select()
-
-    let tickets = {}
-    data.forEach(ticket => {
-        if(ticket.name in tickets) {
-            tickets[ticket.name] += 1
-        } else {
-            tickets[ticket.name] = 1
-        }
-    });
-
-    let salesData = []
-    for (const name in tickets) {
-        salesData.push({name: name, amount: tickets[name]})
+  let tickets = {};
+  data.forEach((ticket) => {
+    if (ticket.name in tickets) {
+      tickets[ticket.name] += 1;
+    } else {
+      tickets[ticket.name] = 1;
     }
+  });
 
-    res.send({ sales: salesData })
+  let salesData = [];
+  for (const name in tickets) {
+    salesData.push({ name: name, amount: tickets[name] });
+  }
+
+  res.send({ sales: salesData });
 };
 
 export default handler;
