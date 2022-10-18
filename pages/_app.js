05ff2bf037b74c109b47ae893187cc50e385cb66
Colin Grob <colin.grob87@gmail.com>
2022-10-17 21:41:14 -0700

    fixed ga

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,12 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script
+      <script
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
-      <Script strategy="lazyOnload">
+      <script strategy="lazyOnload">
         {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
@@ -18,7 +18,7 @@ function MyApp({ Component, pageProps }) {
                     page_path: window.location.pathname,
                     });
                 `}
-      </Script>
+      </script>
       <CartProvider>
         <Component {...pageProps} />
       </CartProvider>
