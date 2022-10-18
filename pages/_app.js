2beeac96252ef3e99cabea3164e348e9975cec4d
Colin Grob <colin.grob87@gmail.com>
2022-10-17 21:39:34 -0700

    added ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,29 @@
-import CartProvider from '../context/cart'
-import '../styles/globals.css'
+import CartProvider from "../context/cart";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <CartProvider><Component {...pageProps} /></CartProvider>
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
+      <CartProvider>
+        <Component {...pageProps} />
+      </CartProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
